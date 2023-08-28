dcd4df8b9921ca217055148b5231eb26c16f0257
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2023-08-28 16:07:55 -0400

    feat(user): implement findByEmail

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,27 +1,29 @@
-import { Injectable } from '@nestjs/common';
-import * as bcrypt from 'bcrypt';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { Injectable } from '@nestjs/common'
+import * as bcrypt from 'bcrypt'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CreateUserDto } from './dto/create-user.dto'
 
 @Injectable()
 export class UserService {
-  constructor(private readonly prisma: PrismaService) {}
+    constructor(private readonly prisma: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto) {
-    const data = {
-      ...createUserDto,
-      password: await bcrypt.hash(createUserDto.password, 10),
-    };
+    async create(createUserDto: CreateUserDto) {
+        const data = {
+            ...createUserDto,
+            password: await bcrypt.hash(createUserDto.password, 10),
+        }
 
-    const createdUser = await this.prisma.user.create({ data });
+        const createdUser = await this.prisma.user.create({ data })
 
-    return {
-      ...createdUser,
-      password: undefined,
-    };
-  }
+        return {
+            ...createdUser,
+            password: undefined,
+        }
+    }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    findByEmail(email: string) {
+        return this.prisma.user.findUnique({
+            where: { email },
+        })
+    }
 }
