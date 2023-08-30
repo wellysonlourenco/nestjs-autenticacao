5617d845e7af8f8b2b28f3a5f63c58ec98d84510
Wéllyson de Lourenço <loiz.wellyson@gmail.com>
2023-08-30 17:05:52 -0400

    implementacao @currentUser

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { AppService } from './app.service';
+import { CurrentUser } from './auth/decorators/current-user.decorator';
+import { IsPublic } from './auth/decorators/is-public.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @IsPublic()
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get("me")
+  getMe(@CurrentUser() user: User){
+    return user;
+  }
+
 }
